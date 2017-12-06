0c9f457d904beba0ae55a5615217a340908f5c99
Connor McCauley <cdmc13@outlook.com>
2017-12-05 21:52:16 -0500

    minor history fix

--- src/server/history.php
+++ src/server/history.php
@@ -9,9 +9,10 @@
     $country  = $_POST['country'];
 	$province = $_POST['province'];
     $city     = $_POST['city'];
+    $type     = $_POST['type'];
     $range    = $_POST['range'];
 
-    if ($range != 'filter') {
+    if ($type != 'filter') {
         switch ($range) {
             case 'day':
                 $from = date("Y-m-d H:i:s", strtotime("-1 day"));            
